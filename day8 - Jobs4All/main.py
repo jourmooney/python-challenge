3e7636a9b77912bcded8ee7004f63f1cd216cb3a
jourmooney <jourmooney@gmail.com>
2022-04-26 00:07:28 +0900

    update assignment

--- day8 - Jobs4All/main.py
+++ day8 - Jobs4All/main.py
@@ -6,23 +6,53 @@ from bs4 import BeautifulSoup
 os.system("clear")
 alba_url = "http://www.alba.co.kr"
 
-# def get_company_links(url):
-#     response = requests.get(alba_url)
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     result = soup.select("div#MainSuperBrand ul.goodsBox li a[href]")
-#     return result
+def get_company_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    result = soup.select("div#MainSuperBrand > ul.goodsBox > li > a.goodsBox-info")
+    return result
 
-# url = get_company_links(alba_url)[0].get('href')
-url = "http://mcdonalds.alba.co.kr/"
+def get_last_page(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    result = soup.select("div#NormalInfo > p.jobCount > strong")[0].get_text(strip=True)
+    last_page = int(result) // 50 + 1
+    return int(last_page)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-result = soup.select("div#SubContents > div#NormalInfo > table > tbody")
-print(result[0])
-# title = html.select("")
+def extract_job(html):
+    place = html.select_one("td.local.first")
+    title = html.select_one("span.company")
+    time = html.select_one("td.data")
+    pay = html.select_one("td.pay")
+    date = html.select_one("td.regDate.last")
+    return {
+        'place': place,
+        'title': title,
+        'time': time,
+        'pay': pay,
+        'date': date,
+    }
 
-# place = html.select_one("span.local").get_text()
-# title = html.select_one("span.company").get_text()
-# time
-# pay
-# date
+def extract_jobs(last_page, url):
+    jobs=[]
+    for page in range(last_page):
+        print(f"Scrapping SO: Page: {page+1}")
+        response = requests.get(f"{url}/job/brand/?page={page+1}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        results = soup.select("div#SubContents > div#NormalInfo > table > tbody > tr:not(.summaryView)")[0]
+        for result in results:
+            print(result)
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+    
+company_urls = get_company_links(alba_url)
+for url in company_urls[:2]:
+    print(f"Scraping {url.select('img')[0]['alt']}")
+    # print(f"Scraping {url.select('span.company')[0].get_text()}")
+    url = url['href']
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
+
+print(jobs)
+
